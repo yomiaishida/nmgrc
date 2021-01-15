dc4176c4d8c15e1f1275f02d884e0d0135ebb52b
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-15 22:09:15 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Game } from "./component/Game";
+
+function App() {
+  return (
+    <div className="App container">
+      <h1>Number Guesser</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
